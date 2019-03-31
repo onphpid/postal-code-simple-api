d36b66767e1e82c6daa1933ba33751cf35a58880
pentagonal <pentagonaldevelopment@gmail.com>
2019-03-31 17:12:58 +0700

    Add bot ignorance

--- app/Middleware.php
+++ app/Middleware.php
@@ -6,13 +6,34 @@ namespace OnPhpId\IndonesiaPostalCode;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Slim\Exception\SlimException;
 use Slim\Http\Environment;
+use Slim\Http\Response;
 use Slim\Http\Uri;
 
 /**
  * @var \Slim\App $this
  */
 $this->add(function (ServerRequestInterface $request, ResponseInterface $response, callable $next) {
+    $ua = $request->getHeaderLine('User-Agent');
+    /**
+     * @var Response $response
+     */
+    if (!preg_match('/^Mozilla\/5\.0/', $ua) // user agent start with mozilla
+        || preg_match('/bot|googlebot|crawler|curl|spider|robot|crawling/i', $ua) // check if contains spider
+    ) {
+        throw new SlimException(
+            $request,
+            $response
+                ->withStatus(403)
+                ->withJson(
+                    [
+                        'message' => 'Bot is not allowed here!'
+                    ]
+                )
+        );
+    }
+
     $uri = $request->getUri();
     /**
      * @var Environment $environment
